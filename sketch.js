189c2328d7b741e4ff162b18ca28f38d16bc76f6
Zhang4202314 <4202314@spsd.sk.ca>
2024-11-18 10:53:12 -0600

    initial commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+// Pinball
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(5, 5, 200);
+
+  noStroke();
+  fill(224, 17, 95);
+  circle(mouseX, mouseY, 50);
+}
