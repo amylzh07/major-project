a9b0f1c6cc0ca1801c58a6027a56f0fac077c7d7
Amy L. Zhang <167574187+amylzh07@users.noreply.github.com>
2024-11-21 10:24:25 -0600

    began spawning machine

--- sketch.js
+++ sketch.js
@@ -2,14 +2,30 @@
 // Amy Lening Zhang
 // January 26, 2024
 
+let midScreen;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  midScreen = {
+    x: windowWidth / 2,
+    y: windowHeight / 2,
+  };
 }
 
 function draw() {
-  background(5, 5, 200);
+  background(50);
 
-  noStroke();
-  fill(224, 17, 95);
-  circle(mouseX, mouseY, 50);
+  spawnMachine();
+}
+
+function spawnMachine() {
+  beginShape();
+  fill(255);
+
+  vertex(midScreen.x - 50, midScreen.y - 80);
+  vertex(midScreen.x + 50, midScreen.y - 80);
+  vertex(midScreen.x - 100, midScreen.y - 150);
+  vertex(midScreen.x + 100, midScreen.y + 150);
+  endShape(CLOSE);
 }
